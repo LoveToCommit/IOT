9d8dc6443594bf704e329b16b239280af1c7f655
Priyanka Mathur <prmathur@microsoft.com>
2016-11-14 15:44:42 -0800

    Update folder structure for java translation

---
+++ java_translation/translation_wrapper/src/main/java/com/microsoft/azure/iothub/RunTime.java
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package com.microsoft.azure.iothub;
+
+/**
+ * Created by prmathur on 11/11/2016.
+ */
+public class RunTime {
+
+}
---
+++ java_translation/translation_wrapper/src/test/java/unit/com/microsoft/azure/iothub/RunTimeTest.java
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package unit.com.microsoft.azure.iothub;
+
+/**
+ * Created by prmathur on 11/11/2016.
+ */
+public class RunTimeTest {
+
+}
